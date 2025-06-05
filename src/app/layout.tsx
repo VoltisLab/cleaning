d9a04305056edd28c209fcd8132a5e256af4c5e4
Stanley <stanley@axumtechnologies.com>
2025-06-05 18:18:01 +0100

    modified layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans, Lato } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import NewsletterSection from "@/components/landing/NewsLetterSection";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <NewsletterSection />
       <Footer />
     </html>
   );
