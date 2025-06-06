ec42e4d52ed969b3b9cbec403654746a8f68e01b
Stanley <stanley@axumtechnologies.com>
2025-06-06 13:51:11 +0100

    Added auth screens

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthForm from '@/components/auth/Auth'
+const Auth = () => {
+  return (
+    <div className='min-h-screen bg-white '>
+      <AuthForm />
+    </div>
+  )
+}
+
+export default Auth
